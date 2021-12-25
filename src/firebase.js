43b1c7635103704a0ecfd8db9afea06970c13011
hsynarslan <hsyn06arslan@gmail.com>
2021-12-25 15:50:31 +0300

    env

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBCrBMWHKytKQ_DJshhphA5c0mdndSVGpg',
-  authDomain: 'disneyplus-clone-2e2f8.firebaseapp.com',
-  projectId: 'disneyplus-clone-2e2f8',
-  storageBucket: 'disneyplus-clone-2e2f8.appspot.com',
-  messagingSenderId: '861445811654',
-  appId: '1:861445811654:web:fefc4ca37d05d70a8996c7'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
